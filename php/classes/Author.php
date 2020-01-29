46ff58429bf20f09e3ff9974ddaec66a6bccedb2
Daniel Gonzales <dgonzales371@cnm.edu>
2020-01-28 19:35:39 -0700

    "I created objects within PhP"

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -1,9 +1,9 @@
 <?php
-namespace dgonzales351\Cnm\DataDesign;
+namespace dgonzales351\DataDesign;
 
-require_once(dirname(__DIR__, 2) . "/vendor/Author.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
-use dgonzales351\Uuid\Uuid;
+use Ramsey\Uuid\Uuid;
 /**
  * Cross Section of an Authors Profile
  *
@@ -67,6 +67,20 @@ private $AuthorId;
 	 * @throws \RangeException if $newProfileId is not positive
 	 * @throws \TypeError if the profile Id is not
 	 **/
+	public function __construct($newAuthorId, $newAuthorUsername, string $newAuthorEmail, $newauthorHash = null) {
+		try {
+			$this->setProfileActivationToken($newAuthorId);
+			$this->setAuthorUsername($newAuthorUsername);
+			$this->setAuthorEmail($newAuthorEmail);
+			$this->setauthorHash($newauthorHash);
+		}
+			//determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
 	public function setProfileId( $authorId): void {
 		try {
 			$uuid = self::validateUuid($authorId);
@@ -124,19 +138,19 @@ private $AuthorId;
 	 * @throws \RangeException if $newAuthorAtHandle is > 32 characters
 	 * @throws \TypeError if $newAuthorAtHandle is not a string
 	 **/
-	public function setPAuthorAtHandle(string $newAuthorAtHandle) : void {
+	public function setAuthorUsername(string $newAuthorUsername) : void {
 		// verify the at handle is secure
-		$newAuthorAtHandle = trim($newAuthorAtHandle);
-		$newAuthorAtHandle = filter_var($newAuthorAtHandle, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newAuthorAtHandle) === true) {
+		$newAuthorUsername = trim($newAuthorUsername);
+		$newAuthorUsername = filter_var($newAuthorUsername, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newAuthorUsername) === true) {
 			throw(new \InvalidArgumentException("authors username is empty or insecure"));
 		}
 		// verify the at handle will fit in the database
-		if(strlen($newA) > 32) {
+		if(strlen($newAuthorUsername) > 32) {
 			throw(new \RangeException("authors username is too large"));
 		}
 		// store the at handle
-		$this->AuthorAtHandle = $newAuthorUsername;
+		$this->AuthorUsername = $newAuthorUsername;
 	}
 	/**
 	 * accessor method for email
@@ -222,11 +236,10 @@ private $AuthorId;
 	 * @throws \RangeException if $newAuthor is > 32 characters
 	 * @throws \TypeError if $newAuthor is not a string
 	 **/
-	public function setAuthorUsername(?string $authorUsername): void {
+	public function authorUsername(?string $authorUsername): void {
 		//if $authorUsername is null return it right away
 		if($authorUsername === null) {
 			$this->authorUsername = null;
-			return;
 		}
 		// verify the author username is secure
 		$authorUsername = trim($authorUsername);
--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,5 +1,8 @@
 <?php
+namespace dgonzales351\DataDesign;
 
-
-
-?>
+require_once(dirname(__DIR__ ) . "/classes/autoload.php");
+use dgonzales351\DataDesign\Author;
+$hash = password_hash( "Fighter2424", PASSWORD_ARGON2I, ["time_cost" => 7]);
+$author = new Author("a4661c33f7134b72bfc097eaa16d3141", "dr463hys83jw82ja93j7sy3h6st3ga32", "https://www.uuidgenerator.net/","dgonzales371@cnm.edu", $hash , "dgonzales371");
+var_dump($author);
